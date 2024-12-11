ddbb5db43d5935ec37598eaff64c2e596cb45551
Rehmat Ullah <rehmatullahbhatti@gmail.com>
2024-12-11 10:22:46 +0500

    instapay payment method php

---
+++ auth.php
@@ -0,0 +1,27 @@
+<?php
+require ('auth_.php');
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<form action="" method="post">
+Order Description =><span style="color:red;">*</span> => <input type="text" name="OrderDescription" value="Some Item" required/> </br> 
+Amount =><span style="color:red;">*</span> => <input type="text" name="Amount" value="50.0" required/> </br> 
+Tax => <input type="text" name="Tax" value="9" readonly/> </br> 
+Shipping Charges => <input type="text" name="Shipping" value="10" readOnly/> </br> 
+Cc Number => <input type="text" name="CcNumber" value="12121212012"/> </br> 
+Cc Exp => <input type="text" name="CcExp" value="1122"/> </br> 
+Cvv => <input type="text" name="Cvv" value="012"/> </br> 
+Company<span style="color:red;">*</span> => <input type="text" name="Company" value="expo company" required/> </br> 
+First Name<span style="color:red;">*</span> => <input type="text" name="FirstName" value="Rehmat" required/> </br> 
+Last Name => <input type="text" name="LastName" value="Ullah"/> </br>
+Address1 => <input type="text" name="Address1" value="h1 s1 lhr"/> </br>
+City => <input type="text" name="City" value="lahore"/> </br>
+State => <input type="text" name="State" value="Punjab"/> </br>
+Zip => <input type="text" name="Zip" value="54000"/> </br>
+Phone => <input type="text" name="Phone" value="12345678"/> </br>
+Email => <input type="email" name="Email" value="abc@gmail.com"/> </br>
+<input type="submit" value="Set order" name="action_button"/> 
+</form>
+</body>
+</html>
---
+++ sale_.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Result is returned as an array in the format of...
+$result = Array
+(
+    [response] => 1
+    [responsetext] => SUCCESS
+    [authcode] => 123456
+    [transactionid] => 1087714082
+    [avsresponse] => Y
+    [cvvresponse] => M
+    [orderid] =>
+    [type] => sale
+    [response_code] => 100
+)
+ *
+ */
+
+require ('lib/nmiDirectPost.class.php');
+if(isset($_POST['action_button'])){
+	$transaction = new nmiDirectPost;
+	$transaction->setOrderDescription($_POST['OrderDescription']);
+	$transaction->setAmount($_POST['Amount']);
+	$transaction->setTax($_POST['Tax']);
+	$transaction->setShipping($_POST['Shipping']);
+	$transaction->setCcNumber($_POST['CcNumber']);
+	$transaction->setCcExp($_POST['CcExp']);
+	$transaction->setCvv($_POST['Cvv']);
+	$transaction->setCompany($_POST['Company']);
+	$transaction->setFirstName($_POST['FirstName']);
+	$transaction->setLastName($_POST['LastName']);
+	$transaction->setAddress1($_POST['Address1']);
+	$transaction->setCity($_POST['City']);
+	$transaction->setState($_POST['State']);
+	$transaction->setZip($_POST['Zip']);
+	$transaction->setPhone($_POST['Phone']);
+	$transaction->setEmail($_POST['Email']);
+	$transaction->sale();
+
+	$result = $transaction->execute();
+
+	switch($result['response'])
+	{	case 1: 
+			echo "Successful";	//Success
+			//Add order to database or whatever other internal action is needed
+			//You would generally then redirect your use to the thankyou page
+			break;
+		default; 
+			echo "Un-Successful"; //Non-Successful
+			//Error or fail, The fail response is actually 2, error is 3, but we fail regardless if it's not successful
+			//You can redisplay your form here, or do something
+	}
+}
+?>
